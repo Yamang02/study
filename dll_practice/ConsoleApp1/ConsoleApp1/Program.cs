8501d2ef4838456559bff530160fd3f5680eaa6c
ljj0210 <ljj0210@gmail.com>
2023-08-09 08:13:43 +0900

    .net practice

---
+++ dll_practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        Program()
+        {
+            System.Console.WriteLine("Program instance created");
+        }
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Main Method started...");
+            Program p1 = new Program();
+        }
+    }
+}
+
