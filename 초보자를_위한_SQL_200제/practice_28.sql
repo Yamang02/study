e77916000f2d5e2c7761e807669cb82a867e31bb
ljj0210 <ljj0210@gmail.com>
2023-09-13 23:57:40 +0900

    SQL

--- 초보자를_위한_SQL_200제/practice_28.sql
+++ 초보자를_위한_SQL_200제/practice_28.sql
@@ -1,42 +1,28 @@
-/* 개월 수 더한 날짜 출력하기 (ADD_MONTHS) */
+/* 특정 날짜 뒤에 오는 요일 날짜 출력하기(NEXT_DAY)  */
 
 SELECT
-    ADD_MONTHS(TO_DATE('2019-05-01', 'RRRR-MM-DD'), 100)
+    '2019/05/22' AS 날짜,
+    NEXT_DAY('2019/05/22', '월요일')
+FROM
+    DUAL;
+
+SELECT
+    SYSDATE AS "오늘_날짜"
 FROM
     DUAL;
     
--- 일수를 더하기 (한 달이 30일이나 31일이므로 더하는 일자를 파악해야 함)
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + 100
-FROM
-    DUAL;
     
--- ADD_MONTH
 SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + INTERVAL '100' MONTH
+    NEXT_DAY(SYSDATE, '화요일') AS 다음_날짜
 FROM
-    DUAL;
-
---    
+    DUAL;    
+    
 SELECT
-    INTERVAL '100' MONTH
+    NEXT_DAY(ADD_MONTHS('2019/05/22', 100), '화요일') AS 다음_날짜
 FROM
-    DUAL;
-
--- 날짜유형 뒤에 주어지는 숫자는 자리수? 기본값이 유형마다 있어 만약 그 범위보다 큰 수치를 주려면 명시해야 하는 듯
+    DUAL;       
+    
 SELECT
-    INTERVAL '3100' DAY ( 4 )
+    NEXT_DAY(ADD_MONTHS(SYSDATE, 100), '월요일') AS 다음_날짜
 FROM
-    DUAL;
-
--- 3 년 더하기
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + INTERVAL '3' YEAR
-FROM
-    DUAL;
-
--- 3년 5개월 후 파악하기
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + TO_YMINTERVAL('03-05') AS 날짜
-FROM
-    DUAL;
+    DUAL;            
--- 초보자를_위한_SQL_200제/practice_29.sql
+++ 초보자를_위한_SQL_200제/practice_29.sql
@@ -1,42 +1,21 @@
-/* 개월 수 더한 날짜 출력하기 (ADD_MONTHS) */
+/* 특정날짜가 있는 달의 마지막 날짜 출력하기 LAST_DAY */
 
 SELECT
-    ADD_MONTHS(TO_DATE('2019-05-01', 'RRRR-MM-DD'), 100)
-FROM
-    DUAL;
-    
--- 일수를 더하기 (한 달이 30일이나 31일이므로 더하는 일자를 파악해야 함)
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + 100
-FROM
-    DUAL;
-    
--- ADD_MONTH
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + INTERVAL '100' MONTH
+    '2019/05/22'           AS 날짜,
+    LAST_DAY('2019/05/22') AS 마지막_날짜
 FROM
     DUAL;
 
---    
 SELECT
-    INTERVAL '100' MONTH
+    LAST_DAY(SYSDATE) - SYSDATE AS "남은 날짜"
 FROM
     DUAL;
 
--- 날짜유형 뒤에 주어지는 숫자는 자리수? 기본값이 유형마다 있어 만약 그 범위보다 큰 수치를 주려면 명시해야 하는 듯
 SELECT
-    INTERVAL '3100' DAY ( 4 )
+    ENAME,
+    HIREDATE,
+    LAST_DAY(HIREDATE)
 FROM
-    DUAL;
-
--- 3 년 더하기
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + INTERVAL '3' YEAR
-FROM
-    DUAL;
-
--- 3년 5개월 후 파악하기
-SELECT
-    TO_DATE('2019-05-01', 'RRRR-MM-DD') + TO_YMINTERVAL('03-05') AS 날짜
-FROM
-    DUAL;
+    EMP
+WHERE
+    ENAME = 'KING';
