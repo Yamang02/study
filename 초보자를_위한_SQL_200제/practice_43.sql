75bc490b577b105fa3c95b7650a31fc8760f2b75
ljj0210 <ljj0210@gmail.com>
2023-09-26 00:08:09 +0900

    SQL

--- 초보자를_위한_SQL_200제/practice_43.sql
+++ 초보자를_위한_SQL_200제/practice_43.sql
@@ -1,25 +1,13 @@
-/* 데이터 분석 함수로 순위 출력하기 ②(DENSE_RANK)*/
--- 동랭크를 넘어가지 않고 계산함
+/* 데이터 분석 함수로 등급 출력하기(NTILE)*/
 SELECT
-    ENAME,
-    JOB,
-    SAL,
-    RANK()
-    OVER(
-        ORDER BY
-            SAL DESC
-    ) AS RANK,
-    DENSE_RANK()
-    OVER(
-        ORDER BY
-            SAL DESC
-    ) AS DENSE_RANK
+    ename,
+    job,
+    sal,
+    NTILE (4) over (
+        order by
+            sal desc nulls last
+    ) 등급
 FROM
     EMP
 WHERE
-    JOB IN ( 'ANALYST', 'MANAGER' );
-
-SELECT
-    DENSE_RANK(2975) WITHIN GROUP(ORDER BY SAL DESC) 순위
-FROM
-    EMP;
+    JOB IN ('ANALYST', 'MANAGER', 'CLERK');
--- 초보자를_위한_SQL_200제/practice_44.sql
+++ 초보자를_위한_SQL_200제/practice_44.sql
@@ -1,25 +1,38 @@
-/* 데이터 분석 함수로 순위 출력하기 ②(DENSE_RANK)*/
--- 동랭크를 넘어가지 않고 계산함
+/* 데이터 분석 함수로 순위의 비율 출력하기(CUME_DIST)*/
+
 SELECT
-    ENAME,
-    JOB,
-    SAL,
+    E.ENAME,
+    E.SAL,
     RANK()
     OVER(
         ORDER BY
-            SAL DESC
+            E.SAL DESC
     ) AS RANK,
     DENSE_RANK()
     OVER(
         ORDER BY
             SAL DESC
-    ) AS DENSE_RANK
+    ) AS DENSE_RANK,
+    CUME_DIST()
+    OVER(
+        ORDER BY
+            SAL DESC
+    ) AS CUM_DIST
 FROM
-    EMP
-WHERE
-    JOB IN ( 'ANALYST', 'MANAGER' );
+    EMP E;
 
 SELECT
-    DENSE_RANK(2975) WITHIN GROUP(ORDER BY SAL DESC) 순위
+    E.ENAME,
+    E.SAL,
+    RANK()
+    OVER(PARTITION BY E.JOB
+         ORDER BY
+             E.SAL DESC
+    ) AS RANK,
+    CUME_DIST()
+    OVER(PARTITION BY JOB
+         ORDER BY
+             SAL DESC
+    ) AS CUM_DIST
 FROM
-    EMP;
+    EMP E;
