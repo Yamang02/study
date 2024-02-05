8794f363620d0bfb32628675961ffeea70c5fd2d
ljj0210 <ljj0210@gmail.com>
2024-02-05 23:46:28 +0900

    SQL144

---
+++ 초보자를_위한_SQL_200제/practice_144.sql
@@ -0,0 +1,14 @@
+/*144. PL/SQL While Loop 이해하기 */
+
+DECLARE V_COUNT NUMBER(10) := 0;
+
+
+BEGIN
+    WHILE V_COUNT < 9 LOOP
+        V_COUNT := V_COUNT + 1;
+        DBMS_OUTPUT.PUT_LINE('2 x'
+                             || V_COUNT
+                             || ' = '
+                             || 2 + V_COUNT);
+    END LOOP;
+END;
