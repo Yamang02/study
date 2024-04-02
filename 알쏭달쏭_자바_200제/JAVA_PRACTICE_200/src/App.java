12b4a55b53c29920619feaf7ecd66585b8aecf1b
Jeongjun <ljj0210@gmail.com>
2024-04-02 15:19:14 +0900

    JAVA2

--- 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/App.java
+++ 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/App.java
@@ -1,17 +1,11 @@
+import practice.chapter1.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Practice1_1 Quiz = new Practice1_1(); // 올바른 객체 생성 방식
-        Quiz.solve();
-    }
-}
+	public static void main(String[] args) throws Exception {
+		Chapter1 Quiz = new Chapter1(); 
 
-class Practice1_1 {
-    public Practice1_1() {
-    }
-
-    public void solve() {
-        System.out.println("첫 Java 프로그램입니다.");
-        System.out.println("화면에 출력하고 있습니다.");
-    }
+		// 1. Quiz.Hello();
+		Quiz.HelloError();
+		
+	}
 }
---
+++ 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/practice/chapter1/Chapter1.java
@@ -0,0 +1,15 @@
+package practice.chapter1;
+
+public class Chapter1 {
+
+	public void Hello() {
+		System.out.println("첫 Java 프로그램입니다.");
+		System.out.println("화면에 출력하고 있습니다.");
+	}
+
+	public void HelloError() {
+		System.out.println("세미콜론이 없으면??")
+		System.out.println("화면에 출력하고 있습니다.");
+	}
+
+}
