7d5e9d855760cbbb7d8c59b076e7ad182c34e675
ljj0210 <ljj0210@gmail.com>
2024-04-03 21:54:10 +0900

    JAVA3

--- 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/App.java
+++ 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/App.java
@@ -1,17 +1,12 @@
+import practice.Chapter1;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Practice1_1 Quiz = new Practice1_1(); // 올바른 객체 생성 방식
-        Quiz.solve();
-    }
-}
+	public static void main(String[] args) throws Exception {
+		Chapter1 Quiz = new Chapter1();
 
-class Practice1_1 {
-    public Practice1_1() {
-    }
-
-    public void solve() {
-        System.out.println("첫 Java 프로그램입니다.");
-        System.out.println("화면에 출력하고 있습니다.");
-    }
+		Quiz.Hello1A();
+		Quiz.Hello1B();
+		Quiz.Hello1C();
+		
+	}
 }
---
+++ 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/practice/Chapter1.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class Chapter1 {
+
+	public void Hello1A() {
+		System.out.println("첫 Java 프로그램입니다. 화면에 출력하고 있습니다.");
+	}
+
+	public void Hello1B() {
+		System.out.print("첫 Java 프로그램입니다. ");
+		System.out.println(" 화면에 출력하고 있습니다.");
+
+	}
+
+	public void Hello1C() {
+		System.out.println("첫 Java 프로그램입니다. " + " 화면에 출력하고 있습니다.");
+
+	}
+
+}
