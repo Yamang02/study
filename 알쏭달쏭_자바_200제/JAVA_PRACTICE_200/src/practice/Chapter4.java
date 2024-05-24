4e50a71e34e8382b22bc9d13cdff8e966ba798f5
Jeonjun_Ideapad3 <ljj0210@gmail.com>
2024-05-24 23:05:21 +0900

    JAVA_

--- 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/practice/Chapter4.java
+++ 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/practice/Chapter4.java
@@ -315,4 +315,20 @@ public class Chapter4 {
 		}
 
 	}
+
+	public void PutAsterisk5B() {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.print("How many * ? : ");
+		int n = stdIn.nextInt();
+		if (n > 0) {
+			for (int i = 0; i < n / 5; i++)
+				System.out.println("*****");
+			int rest = n % 5;
+			if (rest > 0) {
+				for (int i = 0; i < rest; i++)
+					System.out.print("*");
+				System.out.println();
+			}
+		}
+	}
 }
