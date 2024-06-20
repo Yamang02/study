47c3fdf34ff41dbe58ad9e30cbe864330a2ee0a8
LAPTOP_JJ <ljj0210@gmail.com>
2024-06-20 22:30:05 +0900

    JAVA78

--- 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/practice/Chapter5.java
+++ 알쏭달쏭_자바_200제/JAVA_PRACTICE_200/src/practice/Chapter5.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.Scanner;
+
 import util.Utility;
 
 public class Chapter5 {
@@ -13,14 +15,35 @@ public class Chapter5 {
 	}
 
 	public void OctHex() {
-		int x = util.getInputInt();
+		Scanner sc = util.getScanner();
+		Integer x = util.getInputNumber(Integer.class, sc);
 		System.out.printf("8 jinsu is %o \n", x);
 		System.out.printf("16 jinsu is %x \n", x);
 	}
-	
+
 	public void OctHex1Line() {
-		int x = util.getInputInt();
+		Scanner sc = util.getScanner();
+		Integer x = util.getInputNumber(Integer.class, sc);
 		System.out.printf("8 jinsu %o and 16 jinsu %x \n", x, x);
 	}
 
+	public void FloatDoubleScanPrint() {
+		Scanner sc = util.getScanner();
+		System.out.println(" x is float type & y is double type");
+		util.printEnterNumber("x");
+		Float x = util.getInputNumber(Float.class, sc);
+		util.printEnterNumber("y");
+		Double y = util.getInputNumber(Double.class, sc);
+
+		System.out.println("x : " + x);
+		System.out.println("y : " + y);
+	}
+
+	public void PrintBoolean() {
+		boolean b1 = true;
+		boolean b2 = false;
+
+		System.out.println("b1 = " + b1);
+		System.out.println("b2 = " + b2);
+	}
 }
